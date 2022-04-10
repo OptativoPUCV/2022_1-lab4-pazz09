79949bc93ad6936a267dfc9d91a9e3beb539e23d
Your Name <you@example.com>
2022-04-10 10:45:58 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -91,9 +91,11 @@ Pair * searchMap(HashMap * map,  char * key) {
 
     while ( map->buckets[idx] != NULL && map->buckets[idx]->key != NULL){
 
-        if ( is_equal(key,map->buckets[idx]->key) == 1) return map->buckets[idx];
-        idx = (idx + 1) % map->capacity;
+        if ( is_equal(key,map->buckets[idx]->key) == 1){
+            return map->buckets[idx];
+        }
 
+        idx = (idx + 1) % map->capacity;
         return NULL;
         
     }
